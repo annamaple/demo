57a57341523a0ffa53ba06e2831f58a077f4ac71
lei <xionglei1614@163.com>
2022-05-17 22:01:32 +0800

    add(meet-springboot): 添加注释

--- meet-springboot/src/main/java/com/annamaple/meetspringboot/dao/CityRepository.java
+++ meet-springboot/src/main/java/com/annamaple/meetspringboot/dao/CityRepository.java
@@ -3,9 +3,10 @@ package com.annamaple.meetspringboot.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.annamaple.meetspringboot.bean.City;
-import org.springframework.stereotype.Repository;
 
-@Repository
+/**
+ * 实现JPA的一个借口，不需要加@Repostory注解
+ */
 public interface CityRepository extends JpaRepository<City, Integer> {
-	
+
 }
