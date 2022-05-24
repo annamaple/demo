e509e3f5724f4980437e2205d5a873f9b60ef250
熊雷 <xionglei@jiuqi.com.cn>
2022-05-24 16:46:31 +0800

    fix(meeting-springboot): 调整AnnamapleController

--- meet-springboot/src/main/java/com/annamaple/meetspringboot/controller/AnnamapleController.java
+++ meet-springboot/src/main/java/com/annamaple/meetspringboot/controller/AnnamapleController.java
@@ -1,9 +1,13 @@
 package com.annamaple.meetspringboot.controller;
 
+import cn.hutool.core.lang.Console;
 import com.annamaple.meetspringboot.controller.dto.ResultDTO;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 
 /**
@@ -17,7 +21,21 @@ public class AnnamapleController {
 
 
 	@RequestMapping("/hello")
-	public ResultDTO<String> hello() {
+	public ResultDTO<String> hello(HttpServletRequest request) {
+		String header1 = request.getHeader("1");
+		String header2 = request.getHeader("2");
+		String header3 = request.getHeader("3");
+		Console.log("head: {}, {}, {}", header1, header2, header3);
+		try(BufferedReader reader = request.getReader()) {
+			String str;
+			StringBuilder sb = new StringBuilder("");
+			while ((str = reader.readLine()) != null) {
+				sb.append(str);
+			}
+			Console.log("body: {}", sb);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return ResultDTO.success("Hello, World");
 	}
 	
